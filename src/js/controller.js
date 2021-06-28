046ffa82e2bcac0cc549a0f758308f28e453a67f
Tofig Amraslanov <tofiqamraslanov@gmail.com>
2021-06-28 15:32:11 +0400

    Publisher-Subscriber pattern implemented ✅

---
+++ src/js/controller.js
@@ -0,0 +1,36 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const recipeContainer = document.querySelector('.recipe');
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner(recipeContainer);
+
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (e) {
+    alert(e);
+  }
+};
+
+// Subscriber
+const init = function() {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
+ 
+
+
