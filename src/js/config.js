a45784876fe0dbd233e8b37ba9f288b091f5a818
tofigamraslanov <tofiq.amraslanov.t@asoiu.edu.az>
2021-07-07 14:28:47 +0400

    Refactored

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SECONDS = 10;
+export const TIMEOUT_SECONDS = 50;
 export const RESULTS_PER_PAGE = 10;
 export const KEY = 'b15fec1f-c57b-4b81-9431-2a3399b049fa';
 export const MODEL_CLOSE_SECONDS = 2.5;
